918140338c95bed6b2f82b3e44cfcc4d08542ded
Gowtham <gowthamsubramani032@gmail.com>
2025-04-23 11:36:12 +0530

    mod is added in main file

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void main()
     scanf("%d",&a);
     printf("Enter the value of b: ");
     scanf("%d",&b);
-    printf("Press 1 -addition 2-Subraction 3-Multiplication q-exit :");
+    printf("Press 1 -addition 2-Subraction 3-Multiplication 4-division 5-Modulo q-exit :");
     scanf("%c",&ch);
     switch (ch)
     {
@@ -18,6 +18,7 @@ void main()
     case '2':diff(a,b);break;
     case '3':mul(a,b);break;
     case '4':div(a,b);break;
+    case '5':mod(a,b);break;
     case 'q':f=1;break;    
     default:printf("Invalid");break;
     }
