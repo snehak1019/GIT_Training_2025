bf7b22e537cead11a90d6bcd720c413728028594
Gowtham <gowthamsubramani032@gmail.com>
2025-04-23 11:44:11 +0530

    Logical not is added

--- logi.c
+++ logi.c
@@ -7,3 +7,8 @@ int logical_and(int a, int b) {
 int logical_or(int a, int b) {
     return a || b; // Returns 1 if either is non-zero, otherwise 0
 }
+
+int logical_not(int a) {
+    return !a; // Returns 1 if a is 0, otherwise 0
+}
+
