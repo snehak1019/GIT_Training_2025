1d810390e8004826bcfbb65b1e58b998df301b6d
Gowtham <gowthamsubramani032@gmail.com>
2025-04-23 12:02:55 +0530

    Header is added

---
+++ operation.h
@@ -0,0 +1,17 @@
+#ifndef OPERATION_H
+#define OPERATION_H
+
+// Arithmetic Function Declarations
+int sum(int a, int b);         // Addition
+int diff(int a, int b);        // Subtraction
+int mul(int a, int b);         // Multiplication
+int div(int a, int b);         // Division
+int mod(int a, int b);         // Modulo
+
+// Logical Function Declarations
+int logical_and(int a, int b); // Logical AND
+int logical_or(int a, int b);  // Logical OR
+int logical_not(int a);        // Logical NOT
+int logical_xor(int a, int b); // Logical XOR
+
+#endif // OPERATION_H
