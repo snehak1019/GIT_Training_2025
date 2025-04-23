66513f942dfb33a3c07edd2d266c963bb2870c19
Gowtham <gowthamsubramani032@gmail.com>
2025-04-23 10:45:55 +0530

    sum is added in A file

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
  
+int sum(int a,int b)
+{
+    return a+b;
+}
 void main()
 {
     int a=0;
@@ -8,5 +12,6 @@ void main()
     scanf("%d",&a);
     printf("Enter the value of b: ");
     scanf("%d",&b);
+    printf("sum of %d+%d=%d",a,b,sum(a,b));
 
 }
